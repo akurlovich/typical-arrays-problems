6058d27f89f508ec96412fd2ca643b68688986f9
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-02-28 16:33:24 +0300

    fix: delete comments

--- src/index.js
+++ src/index.js
@@ -42,28 +42,3 @@ exports.avg = function avg (array) {
   
 }
 
-
-
-// let avg = 0,
-//     sum = 0;
-// // const array = [-39,30,-32,-9,3,-2,-11,-4,-32,39,0,-14,7,20,-12,22,-23,19,0,2];
-// const array = [1,2,3];
-// if (!(array instanceof Array)) {
-//   console.log('not array');
-// } else if (array.length == 0) {
-//   console.log('not array');
-// } else {
-// const arrLength = array.length;
-// for (let i = 0; i < array.length; i++) {
-//   sum = sum + array[i];
-// }
-// avg = (sum / arrLength);
-
-// console.log(arrLength, sum, avg)
-// console.log(typeof(array))
-// }
-
-// let arr = [1, 5, 3, 0],
-//     newArr = [];
-// newArr = arr.sort(function(a, b){return a - b});
-// console.log(newArr)
