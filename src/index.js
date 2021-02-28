04c142bf16e9f4fadf663efe8743214243b37f87
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-02-28 16:18:00 +0300

    feat: add avg function

--- src/index.js
+++ src/index.js
@@ -8,5 +8,41 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!(array instanceof Array)) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+    let avg = 0,
+        sum = 0;
+    const arrLength = array.length;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    avg = (sum / arrLength);
+    return avg;
+
+  }
+  
 }
+
+
+
+// let avg = 0,
+//     sum = 0;
+// // const array = [-39,30,-32,-9,3,-2,-11,-4,-32,39,0,-14,7,20,-12,22,-23,19,0,2];
+// const array = [1,2,3];
+// if (!(array instanceof Array)) {
+//   console.log('not array');
+// } else if (array.length == 0) {
+//   console.log('not array');
+// } else {
+// const arrLength = array.length;
+// for (let i = 0; i < array.length; i++) {
+//   sum = sum + array[i];
+// }
+// avg = (sum / arrLength);
+
+// console.log(arrLength, sum, avg)
+// console.log(typeof(array))
+// }
